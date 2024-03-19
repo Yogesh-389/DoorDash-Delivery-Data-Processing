dd2b77cc73de14290b476d79963d751f98f2fdd1
Yogesh Tiwari <yogeshtiwari389@gmail.com>
2024-03-20 02:01:51 +0530

    added required files

---
+++ lambda_function.py
@@ -0,0 +1,8 @@
+import json
+import pandas as pd
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
