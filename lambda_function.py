ba4614e4c01dbfeb85ca4bd9211f1225db78a4c7
Yogesh Tiwari <yogeshtiwari389@gmail.com>
2024-03-20 02:25:30 +0530

    all needed files added

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
 import json
 import pandas as pd
 def lambda_handler(event, context):
-    # TODO implement
+    # Read data from landing zone
+    # Transform the data using pandas -- (pandas we using as a layer)
+    # write data into the target zone
+    # push the notification to the SNS Topic
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
